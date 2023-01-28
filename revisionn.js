9cb09f6fd0064a63349867c017edb2b749917b18
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-01-28 18:52:41 +0600

    first commit

--- revisionn.js
+++ revisionn.js
@@ -1,2 +1,25 @@
+//string type variable
 var bottoleFor = 'water';
 var bottoleColor = 'yellow';
+//number type varable
+var sunglassPrice = 120;
+var penColor ='blue';
+//  type varible
+var isExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+
+var items3 = tableItems[3];
+tableItems[4] = 'notebook';
+var penIndex = tableItems.indexOf('pen');
+
+//conditionals
+if(tableItems.length >4){
+
+}
+else if(tableItems[3] == 'pen'){
+
+}
+else{
+    
+}
